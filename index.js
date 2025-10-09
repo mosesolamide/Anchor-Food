fea79ad1e01ff45cc489cb1a1f706a4872ef05b4
mosesolamide <olamidemoses132@gmail.com>
2025-10-09 15:41:29 +0100

    Landing Page done

---
+++ index.js
@@ -0,0 +1,6 @@
+const harmburgerMenu = document.getElementById("harmburger")
+const navList = document.getElementById("mobile-nav-list")
+
+harmburgerMenu.addEventListener("click", () => {
+    navList.classList.toggle("hidden")
+})
